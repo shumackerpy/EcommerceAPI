468192a3c354d45509e5d901200df2d491b978ef
Shumacker Lorenzo <shumackerlorenzo@gmail.com>
2026-05-21 09:32:59 -0400

    commit

--- EcommerceAPI/Controllers/AuthController.cs
+++ EcommerceAPI/Controllers/AuthController.cs
@@ -22,7 +22,7 @@ namespace EcommerceAPI.Controllers
             _context = context;
             _config = config;
         }
-         
+          
         // REGISTRO 
         [HttpPost("register")]
         public async Task<IActionResult> Register(User user)
@@ -78,4 +78,3 @@ namespace EcommerceAPI.Controllers
 }
 
  
-  
